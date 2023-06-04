35b2db9c7908ba2fe00f7b171963b43cf1f68467
Abror <khaitboboev94@mail.ru>
2023-06-04 09:00:57 +0300

    pom file

--- src/main/java/com/org/example/util/MealsUtils.java
+++ src/main/java/com/org/example/util/MealsUtils.java
@@ -1,10 +1,18 @@
 package com.org.example.model;
 
+import com.org.example.util.TimeUtil;
+import sun.util.locale.provider.TimeZoneNameUtility;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class MealsUtils {
     public static void main(String[] args) {
@@ -19,7 +27,12 @@ public class MealsUtils {
         getFilteredWithExcess(meals, LocalTime.of(7,0), LocalTime.of(12, 0), 2000);
     }
     public static List<MealTo> getFilteredWithExcess(List<Meal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO: 03.06.2023
-        return null;
+        Map<LocalDate, Integer> caloriesSumByDate = meals.stream()
+                .collect(
+                        Collectors.groupingBy(Meal::getDate, Collectors.summingInt(Meal::getCalories))
+                );
+        meals.stream()
+                .filter(meal -> TimeUtil.isBetween(meal.getTime(), startTime, endTime))
+                .map(meal -> )
     }
 }
---
+++ src/main/java/com/org/example/util/TimeUtil.java
@@ -0,0 +1,2 @@
+package com.org.example.util;public class TimeUtil {
+}
